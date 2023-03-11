528ed079fa10da672b655ab53cbe8ab43cb7a2bf
VahidehHashempour <127569295+VahidehHashempour@users.noreply.github.com>
2023-03-10 18:23:11 -0600

    Add files via upload

---
+++ DateTimeTransformation.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+# current dateTime
+now = datetime.now()
+
+# convert to date String
+date = now.strftime("%d/%m/%Y")
+print('Date String:', date)
+
+date = now.strftime("%d-%m-%Y")
+print('Date String:', date)
+
+# convert to time String
+time = now.strftime("%H:%M:%S")
+print('Time String:', time)
+
+time = now.strftime("%I:%M:%S %p")
+print('Time String:', time)
