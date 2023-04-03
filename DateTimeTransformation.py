06d1525d9343bc2b35a12ff229109f3e264397e0
VahidehHashempour <127569295+VahidehHashempour@users.noreply.github.com>
2023-04-02 21:33:15 -0500

    Update DateTimeTransformation.py

--- DateTimeTransformation.py
+++ DateTimeTransformation.py
@@ -1,26 +1,36 @@
 from datetime import datetime
 
-# Variable now stores the current dateTime
-now = datetime.now()
-
-# Variable date stores date value in variable now converted to date String format 1
+# Function to return current date and time
+def current_date_time():
+    return datetime.now()
+  
+# Function to return datetime string format 1 from a datetime object
 # New feature name of day in the week added
-date = now.strftime("%d/%m/%Y %A")
-
-print('Date String:', date)
-
-# Variable date stores date value in variable now converted to date String format 2
+def datetime_string_1(datetime_object):
+    return datetime_object.strftime("%d/%m/%Y %A %H:%M:%S")
+  
+# Function to return datetime string format 2 from a datetime object
 # New feature name of day in the week added
-date = now.strftime("%d-%m-%Y %A")
+def datetime_string_2(datetime_object):
+    return datetime_object.strftime("%d-%m-%Y %A %H:%M:%S") 
+  
+# Function to return datetime string format 3 from a datetime object
+# New feature name of day in the week added
+def datetime_string_3(datetime_object):
+    return datetime_object.strftime("%d/%m/%Y %A %I:%M:%S %p")  
+  
+# Function to return datetime string format 4 from a datetime object
+# New feature name of day in the week added
+def datetime_string_4(datetime_object):
+    return datetime_object.strftime("%d-%m-%Y %A %I:%M:%S %p")  
+  
+# Function to print date and time in 4 different formats
+def print_datetime(datetime_object):
+    print('Date and Time String 1:', datetime_string_1(datetime_object))
+    print('Date and Time String 2:', datetime_string_2(datetime_object))
+    print('Date and Time String 3:', datetime_string_3(datetime_object))
+    print('Date and Time String 4:', datetime_string_4(datetime_object))
 
-print('Date String:', date)
+now = current_date_time()
+print_datetime(now)
 
-# Variable time stores time value in variable now converted to time String format 1
-time = now.strftime("%H:%M:%S")
-
-print('Time String:', time)
-
-# Variable time stores time value in variable now converted to time String format 2
-time = now.strftime("%I:%M:%S %p")
-
-print('Time String:', time)
