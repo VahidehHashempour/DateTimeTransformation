9bb6f8422f0b156142d5df15abe75a7371cc9426
VahidehHashempour <127569295+VahidehHashempour@users.noreply.github.com>
2023-04-02 21:42:54 -0500

    Update UnitTesting.py

--- UnitTesting.py
+++ UnitTesting.py
@@ -1,2 +1,26 @@
 #Unit Testing
+import unittest
+from datetime import datetime
 
+class TestGetDateTime(unittest.TestCase):
+
+    def test_date(self):
+        self.assertEqual(current_date_time(), datetime.now())
+        
+        
+class TestDateTimeFormat(unittest.TestCase): 
+  
+    def test_date_format1(self):
+        self.assertEqual(datetime_string_1(datetime.now()), datetime.now().strftime("%d/%m/%Y %A %H:%M:%S"))
+        
+    def test_date_format2(self):
+        self.assertEqual(datetime_string_2(datetime.now()), datetime.now().strftime("%d-%m-%Y %A %H:%M:%S"))
+        
+    def test_date_format3(self):
+        self.assertEqual(datetime_string_3(datetime.now()), datetime.now().strftime("%d/%m/%Y %A %I:%M:%S %p"))
+        
+    def test_date_format4(self):
+        self.assertEqual(datetime_string_4(datetime.now()), datetime.now().strftime("%d-%m-%Y %A %I:%M:%S %p"))    
+
+if __name__ == '__main__':
+    unittest.main()
