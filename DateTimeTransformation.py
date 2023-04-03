0a752ec0a9e3d86d46af612022c3ba283b82de8b
VahidehHashempour <127569295+VahidehHashempour@users.noreply.github.com>
2023-04-02 20:45:16 -0500

    Update DateTimeTransformation.py

--- DateTimeTransformation.py
+++ DateTimeTransformation.py
@@ -1,18 +1,26 @@
 from datetime import datetime
 
-# current dateTime
+# Variable now stores the current dateTime
 now = datetime.now()
 
-# convert to date String
+# Variable date stores date value in variable now converted to date String format 1
+# New feature name of day in the week added
 date = now.strftime("%d/%m/%Y %A")
+
 print('Date String:', date)
 
+# Variable date stores date value in variable now converted to date String format 2
+# New feature name of day in the week added
 date = now.strftime("%d-%m-%Y %A")
+
 print('Date String:', date)
 
-# convert to time String
+# Variable time stores time value in variable now converted to time String format 1
 time = now.strftime("%H:%M:%S")
+
 print('Time String:', time)
 
+# Variable time stores time value in variable now converted to time String format 2
 time = now.strftime("%I:%M:%S %p")
+
 print('Time String:', time)
