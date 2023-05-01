736ca6c3c132d71803f84a94ee347f97f276c37d
VahidehHashempour <127569295+VahidehHashempour@users.noreply.github.com>
2023-04-30 21:52:05 -0500

    Update UnitTesting.py

--- UnitTesting.py
+++ UnitTesting.py
@@ -24,7 +24,14 @@ class TestDateTimeFormat(unittest.TestCase):
     
     # Check to see if the return value of datetime_string_1() is in the form of "%d-%m-%Y %A %I:%M:%S %p"
     def test_date_format4(self):
-        self.assertEqual(datetime_string_4(datetime.now()), datetime.now().strftime("%d-%m-%Y %A %I:%M:%S %p"))    
+        self.assertEqual(datetime_string_4(datetime.now()), datetime.now().strftime("%d-%m-%Y %A %I:%M:%S %p"))  
+        
+class TestDaylightDateTime(unittest.TestCase):
+    # Check to see if the return value of daylight_date_time_start() is less than the return value of daylight_date_time_end()"
+    def test_daylight_start_end(self):
+        # error message in case if test case got failed
+        message = "Daylight start date time is not less than second daylight end date time."
+        self.assertGreater(daylight_date_time_end(), daylight_date_time_start(), message)        
 
 if __name__ == '__main__':
     unittest.main()
