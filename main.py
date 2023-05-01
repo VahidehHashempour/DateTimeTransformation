d5db964b8e016a71ef8dab13631fbd0879c45f6e
VahidehHashempour <127569295+VahidehHashempour@users.noreply.github.com>
2023-04-30 21:50:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,16 @@ def datetime_string_3(datetime_object):
 # New feature name of day in the week added
 def datetime_string_4(datetime_object):
     return datetime_object.strftime("%d-%m-%Y %A %I:%M:%S %p")  
+
+# Function to read daylight-saving starting date time
+def daylight_date_time_start():
+    daylight_saving_starting = input("Insert a date for daylight-saving starting date time as mm/dd/yyyy hh:mm\n")
+    return datetime.strptime(daylight_saving_starting, '%m/%d/%Y %H:%M')
+
+# Function to read daylight-saving ending date time
+def daylight_date_time_end():
+    daylight_saving_ending = input("Insert a date for daylight-saving ending date time as mm/dd/yyyy hh:mm\n")
+    return datetime.strptime(daylight_saving_ending, '%m/%d/%Y %H:%M')
   
 # Function to print date and time in 4 different formats
 def print_datetime(datetime_object):
@@ -35,7 +45,15 @@ def print_datetime(datetime_object):
     print('Date and Time String 2:', datetime_string_2(datetime_object))
     print('Date and Time String 3:', datetime_string_3(datetime_object))
     print('Date and Time String 4:', datetime_string_4(datetime_object))
+    
+# Function to print daylight-saving starting date time and daylight-saving ending date time
+def print_daylight_saving_start_end():
+    startDateTime = daylight_date_time_start();
+    endDateTime = daylight_date_time_end();
+    print('Daylight-saving starting date time: ', startDateTime)
+    print('Daylight-saving ending date time: ', endDateTime)      
 
 now = current_date_time()
+print_daylight_saving_start_end()
 print_datetime(now)
 
